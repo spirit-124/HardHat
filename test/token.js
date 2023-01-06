e7383552eee976bf6044ccdedfba85b423a7f9ef
Bajrang Agrawal <bajrangofficial580@gmail.com>
2023-01-06 21:03:39 +0530

    [modified] token.js

--- test/token.js
+++ test/token.js
@@ -1,32 +1,35 @@
+const { inputToConfig } = require("@ethereum-waffle/compiler");
 const { expect } = require("chai");
-// const { ethers } = require("hardhat");
+const { ethers } = require("ethers");
 
 describe("Token contract", function () {
-  it("Deployment should assign the total supply of tokens to the owner", async function () {
-    const [owner] = await ethers.getSigners();
+  let Token;
+  let hardhatToken;
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
 
-    const Token = await ethers.getContractFactory("Token");
-
-    const hardhatToken = await Token.deploy();
-
-    const ownerBalance = await hardhatToken.balanceOf(owner.address); //ownerBalance = 10000
-    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance); //totalSupply = 10000
+  beforeEach(async function () {
+    Token = await ethers.getContractFactory("Token");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+    hardhatToken = await Token.deploy();
   });
-  it("Should transfer tokens between accounts", async function () {
-    const [owner, addr1, addr2] = await ethers.getSigners();
 
-    const Token = await ethers.getContractFactory("Token");
-
-    const hardhatToken = await Token.deploy();
-
-    // tranfer 10 tokens from owner to addr1
-
-    await hardhatToken.transfer(addr1.address);
-    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(10);
-
-    // transafer 5 tokens form addr1 to addr2
-
-    await hardhatToken.connect(addr1).transfer(addr2.address, 5);
-    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(5);
+  describe("Deployment", function () {
+    it("Should set the right owner", async function () {
+      expect(await hardhatToken.owner()).to.equal(owner.address);
+    });
+    it("Should supply total amount of tokens to owner", async function () {
+      const ownerBalance = await hardhatToken.balanceOf(owner.address);
+      expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    });
+  });
+  describe("transactions", function () {
+    it("Should transfer tokens between accounts", async function () {
+      // Owner account to address 1 account
+      await hardhatToken.transfer(addr1.address, 5);
+      const addr1Balance = await hardhatToken.balanceOf(addr1.address);
+    });
   });
 });
